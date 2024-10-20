04e7ac7e4900839bab18d329a644e863607861e0
Patrick de Azevedo Ferreira <patrickdeazevedo@gmail.com>
2024-10-20 11:53:11 -0300

    Deploy da manhão de domingo 20/10/2024

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 lista_usuarios = ["Patrick", "João", "Augusto", "Marcelo", "Clarisse"]
 
 @app.route("/")
-def hello_world():
+def home():
     return render_template('home.html')
 
 @app.route("/contato")
@@ -15,6 +15,10 @@ def contato():
 @app.route('/usuarios')
 def usuarios():
     return render_template('usuarios.html', lista_usuarios=lista_usuarios)
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
 
 # debug=True -> garante que rode sem precisar pausar e rodar de novo
 if __name__ == '__main__':
